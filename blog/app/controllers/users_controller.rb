9ee1260bac8188a172071269a4f12cfb1cb6b191
Mario Borras <mario.borras@icloud.com>
2018-09-15 17:24:34 -0400

    updated post edit/delete
    Posts can only be deleted/edited by the person who made the post

--- blog/app/controllers/users_controller.rb
+++ blog/app/controllers/users_controller.rb
@@ -14,7 +14,7 @@ class UsersController < ApplicationController
     end
 
     def create
-        @user = User.new(user_pOrams)
+        @user = User.new(user_params)
         if @user.save
             flash[:notice] = "User created!"
             redirect_to @user
